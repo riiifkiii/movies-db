05b322add6d8903820e12cc39ae283b5d85cfc0c
rifkit <rieyfkiey@gmail.com>
2022-10-27 20:34:58 +0700

    menabahkan splide ke 2

--- src/scripts/splider.js
+++ src/scripts/splider.js
@@ -2,9 +2,14 @@ import '/node_modules/@splidejs/splide/dist/css/splide.min.css'
 import Splide from '@splidejs/splide';
 
 
-new Splide('.splide', {
+new Splide('.splide1', {
     type: 'loop',
     autoplay: 'play',
 }).mount();
+
+new Splide('.splide2',{
+    type: 'loop',
+    autoplay: 'pause'
+}).mount()
 
 export default Splide
